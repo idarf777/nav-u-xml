4d05e93d8092659967a67325955793b1c1b289e7
Takeshi Aida <aida@shizentai-factory.com>
2022-08-04 17:40:04 +0900

    zip出力

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # nav-u-xml
-SONY nav-u (NV-U75VT, etc)のユーザ登録地点をエクスポートしたXMLを変換する
+
+## Overview
+SONYボータブルナビnav-uシリーズ(NV-U75VT, etc)のユーザ登録地点をCSVに変換して出力する。
+
+[NaviCon](https://play.google.com/store/apps/details?id=jp.co.denso.navicon.view)で取り込む用のZIPファイルを出力可能なので、Android端末があればNaviCon経由でnav-uから別のナビに登録地点を転送できる。
+
+### Requirements
+
+- python 3.x
+
+## Getting Started
+
+### ユーザ登録地点のエクスポート
+
+1. nav-uでメモリースティックduoに登録地点をエクスポートする。
+2. PCでメモリースティックduoを読み込み、mark_export.xmlをPCのローカルドライブにコピーする。
+
+### 単一のCSVに出力
+
+````
+python ./main.py mark_export.xml
+````
+
+次のような出力を得る。緯度経度は世界測地系である。
+
+```
+Category,Latitude,Longitude,Name
+"買い物",34.605316,138.224303,"海鮮なぶら市場"
+"買い物",35.118235,138.839977,"ららぽーと沼津"
+```
+
+
+### NaviCon用ZIPファイルに出力
+
+````
+python ./main.py mark_export.xml -zip out.zip
+````
+
+#### ZIPファイルの取り込み
+
+1. ZIPファイルを端末に転送
+
+   - Bluetooth転送やgoogleドライブ等を経由してAndroid端末にout.zipを転送する。
+
+2. NaviConにZIPを取り込む
+
+   - [File](https://play.google.com/store/apps/details?id=com.google.android.apps.nbu.files)等のファイラーアプリでout.zipを「共有」し、共有先にNaviConを選択する。
+
+
+:::note warn
+ここではNaviConおよびその操作方法については関知いたしません。
+:::
+
+
+## Author
+
+[twitter](https://twitter.com/idarf777)
+
+## License
+
+[GPL3](./LICENSE)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import xml.etree.ElementTree as Et
 import zipfile
 
 
-def read_xml(xml) -> dict:
+def read_xml(xml: str) -> dict:
     result = {}
     root = Et.fromstring(xml)
     for gpoi in root.iter('gpoi'):
@@ -20,19 +20,36 @@ def read_xml(xml) -> dict:
         # print("%f %f %s %s" % (lat, lon, name, category))
         result.setdefault(category, [])
         result[category].append({'lat': lat, 'lon': lon, 'name': name})
-
     return result
 
 
-def convert_geo_t2w(geo) -> dict:
+def convert_geo_t2w(geo: dict) -> dict:
     lat = geo['lat']
     lon = geo['lon']
     return {'lat': lat - 0.00010695*lat + 0.000017464*lon + 0.0046017,
             'lon': lon - 0.000046038*lat - 0.000083043*lon + 0.010040}
 
 
-def sanitize(s: str) -> str:
-    return re.sub('[",\n\r]', ' ', s)
+# ファイルパスに使えない文字を全角に変換する
+PATH_SANITIZER = str.maketrans({
+    '\\': '＼',
+    '/': '／',
+    ':': '：',
+    '*': '＊',
+    '?': '？',
+    '"': '”',
+    "<": "＜",
+    ">": "＞",
+    "|": "｜",
+    " ": "　"
+})
+CSV_SANITIZER = str.maketrans({
+    '"': '”'
+})
+
+
+def sanitize(s: str, is_file: bool = True) -> str:
+    return re.sub(r'\s', '　', s.translate(PATH_SANITIZER)) if is_file else re.sub(r'\s', ' ', s.translate(CSV_SANITIZER))
 
 
 def main():
@@ -41,33 +58,26 @@ def main():
     # parser.add_argument('-b', '--bool-data', action='store_true', help='')
     # parser.add_argument('-c', '--counter', type=int, const=50, nargs='?', help='')
     # parser.add_argument('userid', type=int, help='')
-    parser.add_argument('-g', '--googlemapsurl', action='store_true', help='google mapsのURLを出力する')
-    parser.add_argument('-z', '--zip', help='zipファイルパス')
+    parser.add_argument('-z', '--zipfile', help='NaviConで読み込む用のZIPファイル出力先')
     parser.add_argument('file', nargs='?', type=argparse.FileType('r', encoding = 'utf8'), default=sys.stdin)
     args = parser.parse_args()
-    is_url = args.googlemapsurl
-    zipout = args.zip
-
-    buf = []
+    outfile = args.zipfile
     pois = read_xml(args.file.read())
-    if is_url:
-        buf.append("タイトル,メモ,URL")
+    if outfile:
+        with zipfile.ZipFile(outfile, 'w', compression=zipfile.ZIP_DEFLATED) as zf:
+            for category, hash_list in iter(sorted(pois.items())):
+                with zf.open(f'{sanitize(category)}.csv', 'w') as f:
+                    buf = ["タイトル,メモ,URL,コメント"]
+                    for h in hash_list:
+                        wgs = convert_geo_t2w(h)
+                        buf.append('"%s",,"https://www.google.com/maps/search/%.6f,%.6f",' % (sanitize(h['name']), wgs['lat'], wgs['lon']))
+                    f.write('\n'.join(buf).encode())
     else:
-        buf.append("Category,Latitude,Longitude,Name")
-    for k,v in iter(sorted(pois.items())):
-        for h in v:
-            wgs = convert_geo_t2w(h)
-            if is_url:
-                buf.append("%s,,https://www.google.com/maps/@%.6f,%.6f,20z" % (sanitize(h['name']), wgs['lat'], wgs['lon']))
-            else:
-                buf.append("%s,%f,%f,%s" % (sanitize(k), wgs['lat'], wgs['lon'], sanitize(h['name'])))
-
-    if zipout:
-        with zipfile.ZipFile(zipout, 'w', compression=zipfile.ZIP_DEFLATED) as zf:
-            with zf.open('imported_nav-u.csv', 'w') as f:
-                f.write('\n'.join(buf).encode())
-    else:
-        print('\n'.join(buf))
+        print("Category,Latitude,Longitude,Name")
+        for category, hash_list in iter(sorted(pois.items())):
+            for h in hash_list:
+                wgs = convert_geo_t2w(h)
+                print('"%s",%f,%f,"%s"' % (sanitize(category, is_file = False), wgs['lat'], wgs['lon'], sanitize(h['name'], is_file = False)))
 
 
 if __name__ == '__main__':
